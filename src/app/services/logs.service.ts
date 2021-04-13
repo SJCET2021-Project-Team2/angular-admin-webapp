bad074dfa4f97c4c9311a7770e8255f5ad011446
AugustineAykara <augustineaykara@gmail.com>
2021-04-13 10:56:46 +0530

    home page layout

---
+++ src/app/services/logs.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogsService {
+
+  constructor() { }
+
+  getUserLogs(){
+    return null;
+  }
+
+}
